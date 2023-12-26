2f2ceee24477770faa95fc4b90197c8422c4e5c2
SahilDhorajiya <121150782+sahil-swb@users.noreply.github.com>
2023-12-26 11:10:57 +0530

    changes in sartaj

--- components/seo/SEO.js
+++ components/seo/SEO.js
@@ -4,7 +4,7 @@ const SEO = ({ title, description, keywords }) => {
   console.log(title, description, keywords);
   return (
     <Head>
-      <title>{`https://sartaj.vercel.app | ${"t11111111111111111111111111111111111111111111111111111111111"}`}</title>
+      <title>{`sartaj | ${"t11111111111111111111111111111111111111111111111111111111111"}`}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
     </Head>
