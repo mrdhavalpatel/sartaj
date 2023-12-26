ff5d400b64a279d223e2ecf60a49b57d451a2490
SahilDhorajiya <121150782+sahil-swb@users.noreply.github.com>
2023-12-26 11:07:34 +0530

    changes in seo

--- components/seo/SEO.js
+++ components/seo/SEO.js
@@ -4,9 +4,9 @@ const SEO = ({ title, description, keywords }) => {
   console.log(title, description, keywords);
   return (
     <Head>
-      <title>{`https://sartaj.vercel.app/products | ${title}`}</title>
+      <title>{`https://sartaj.vercel.app | ${"t11111111111111111111111111111111111111111111111111111111111"}`}</title>
       <meta name="description" content={description} />
-      <meta name="keywords" content={keywords}></meta>
+      <meta name="keywords" content={keywords} />
     </Head>
   );
 };
