80fd922ad4b708db98ecff7145326220741126ff
Love chauhan <info@cipherbrains.com>
2024-02-19 16:23:58 +0530

    Live Server On Sartaj Update
    APi  Update And URL Update to Server Domain

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-    ? "http://localhost:3000"
-    : "https://sartaj.vercel.app";
+export const server = dev ? "http://localhost:3000" : "https://sartajfoods.jp";
