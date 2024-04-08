0a8fa2f28b46fcc5c716a18792c076992658b297
Dhaval <info@cipherbrains.com>
2024-04-08 10:36:01 +0530

    Add Meta Pixel Code
    Add Meta Pixel Code

--- pages/_document.js
+++ pages/_document.js
@@ -17,8 +17,37 @@ class MyDocument extends Document {
                 })(window,document,'script','dataLayer','GTM-KC932TF');
               `,
             }}
-          />
+          ></script>
           {/* End of GTM Container Snippet */}
+
+          {/* Meta Pixel Code Container Snippet */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '457672752429873');
+              fbq('track', 'PageView');
+              `,
+            }}
+          ></script>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+              <img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=457672752429873&ev=PageView&noscript=1"
+/>
+              `,
+            }}
+          ></noscript>
+          {/* End of Meta Pixel Code Container Snippet */}
+
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
